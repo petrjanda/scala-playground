09061f12a32582c9eb1d9ff793b431a1fc947ed9
Petr Janda <petrjandacz@gmail.com>
2015-05-30 22:07:25 +0100

    catch actor initiation error

--- src/main/scala/com/ngneers/Runner.scala
+++ src/main/scala/com/ngneers/Runner.scala
@@ -1,14 +1,27 @@
 package com.ngneers
 
-import akka.actor.{Actor, Props}
+import akka.actor.SupervisorStrategy.{Escalate, Stop}
+import akka.actor.{ActorInitializationException, OneForOneStrategy, Actor, Props}
 import com.ngneers.processors.{File2KafkaProcessor, Kafka2CassandraProcessor}
 import com.softwaremill.react.kafka.ReactiveKafka
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 object Runner {
   def props()(implicit kafka:ReactiveKafka) = Props(new Runner())
 }
 
 class Runner()(implicit kafka:ReactiveKafka) extends Actor {
+  override val supervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
+      case ex: ActorInitializationException => {
+        context.system.shutdown()
+
+        Stop
+      }
+      case _: Exception => Escalate
+    }
+
   def receive = {
     case args@Kafka2CassandraProcessor.Args(_) => run(Kafka2CassandraProcessor.props(args))
     case args@File2KafkaProcessor.Args(_, _) => run(File2KafkaProcessor.props(args))
